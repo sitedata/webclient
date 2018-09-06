a3ad5a30af4f1825c184a62c972d476f489c05d8
Zain Zafar <zain@aurorasolutions.io>
2018-09-06 23:47:47 +0500

    change pgp-workers to use ES5 code for IE11

--- src/assets/static/pgp-worker-encrypt.js
+++ src/assets/static/pgp-worker-encrypt.js
@@ -1,30 +1,35 @@
+'use strict'; // to make UMD bundles work
+
 self.window = {crypto: self.crypto}; // to make UMD bundles work
 
 importScripts('/openpgp.min.js');
 var openpgp = window.openpgp;
 
-onmessage = function (event) {
+
+onmessage = function onmessage(event) {
     if (event.data.encrypt) {
-        encryptContent(event.data.content, event.data.publicKeys).then(data => {
-            postMessage({encryptedContent: data, encrypted: true, callerId: event.data.callerId});
-        })
-    }
-    else if (event.data.encryptSecureMessageReply) {
-	    encryptContent(event.data.content, event.data.publicKeys).then(data => {
-		    postMessage({encryptedContent: data, encryptSecureMessageReply: true});
-	    })
+        encryptContent(event.data.content, event.data.publicKeys).then(function (data) {
+            postMessage({ encryptedContent: data, encrypted: true, callerId: event.data.callerId });
+        });
+    } else if (event.data.encryptSecureMessageReply) {
+        encryptContent(event.data.content, event.data.publicKeys).then(function (data) {
+            postMessage({ encryptedContent: data, encryptSecureMessageReply: true });
+        });
     }
 };
 
-async function encryptContent(data, publicKeys) {
-    if (!data) {
-        return null;
-    }
-    const options = {
-        data: data,
-        publicKeys: publicKeys.map(item => openpgp.key.readArmored(item).keys[0])
-    };
-    return openpgp.encrypt(options).then(ciphertext => {
-        return ciphertext.data;
+function encryptContent (data, publicKeys) {
+    return new Promise(function(resolve, reject){
+        if (!data) {
+            resolve(null);
+        }
+        const options = {
+            data: data,
+            publicKeys: publicKeys.map(function(item){ return openpgp.key.readArmored(item).keys[0];})
+        };
+        
+        openpgp.encrypt(options).then(function(ciphertext) {
+            resolve(ciphertext.data);
+        })
     })
 }
--- src/assets/static/pgp-worker.js
+++ src/assets/static/pgp-worker.js
@@ -1,4 +1,6 @@
-self.window = {crypto: self.crypto}; // to make UMD bundles work
+'use strict';
+
+self.window = { crypto: self.crypto || self.msCrypto }; // to make UMD bundles work
 
 importScripts('/openpgp.min.js');
 var openpgp = window.openpgp;
@@ -6,30 +8,31 @@ var openpgp = window.openpgp;
 var decryptedPrivKeyObj;
 var decryptedSecureMsgPrivKeyObj;
 
-onmessage = function (event) {
+
+onmessage = function onmessage(event) {
     if (event.data.clear) {
         decryptedPrivKeyObj = null;
     }
     else if (event.data.generateKeys) {
-        generateKeys(event.data.options).then((data) => {
+        generateKeys(event.data.options).then(function (data) {
             postMessage({generateKeys: true, keys: data, callerId: event.data.callerId, forEmail: !!event.data.forEmail});
         })
     }
     else if (event.data.decryptSecureMessageKey) {
 	    decryptedSecureMsgPrivKeyObj = openpgp.key.readArmored(event.data.privKey).keys[0];
 	    decryptedSecureMsgPrivKeyObj.decrypt(event.data.password)
-		    .then(res => {
+		    .then(function (res) {
 			    postMessage({decryptSecureMessageKey: true, decryptedKey: decryptedSecureMsgPrivKeyObj});
 		    })
-		    .catch(error => {
-			    postMessage({decryptSecureMessageKey: true, error: error.message});
+		    .catch(function (error) {
+			    postMessage({ decryptSecureMessageKey: true, error: error.message});
 		    });
     }
     else if (event.data.decryptSecureMessageContent) {
 	    if (!event.data.content) {
 		    postMessage({decryptedContent: event.data.content, decryptSecureMessageContent: true});
 	    } else {
-		    decryptContent(event.data.content, decryptedSecureMsgPrivKeyObj).then((data) => {
+		    decryptContent(event.data.content, decryptedSecureMsgPrivKeyObj).then(function (data) {
 			    postMessage({decryptedContent: data, decryptSecureMessageContent: true});
 		    })
 	    }
@@ -37,13 +40,13 @@ onmessage = function (event) {
     else if (!decryptedPrivKeyObj) {
         decryptedPrivKeyObj = openpgp.key.readArmored(event.data.privkey).keys[0];
         decryptedPrivKeyObj.decrypt(event.data.user_key);
-        postMessage({key: decryptedPrivKeyObj});
+        postMessage({ key: decryptedPrivKeyObj});
     }
     else if (event.data.decrypt) {
         if (!event.data.content) {
             postMessage({decryptedContent: event.data.content, decrypted: true, callerId: event.data.callerId});
         } else {
-            decryptContent(event.data.content, decryptedPrivKeyObj).then((data) => {
+            decryptContent(event.data.content, decryptedPrivKeyObj).then(function (data) {
                 postMessage({decryptedContent: data, decrypted: true, callerId: event.data.callerId});
             })
         }
@@ -51,18 +54,21 @@ onmessage = function (event) {
 }
 
 function decryptContent(data, privKeyObj) {
-    const options = {
+    var options = {
         message: openpgp.message.readArmored(data),
         privateKeys: [privKeyObj]
     };
 
-    return openpgp.decrypt(options).then(plaintext => {
+    return openpgp.decrypt(options).then(function (plaintext) {
         return plaintext.data;
+    }).catch(function (error) {
+        console.log(error);
+        return error;
     })
 }
 
 function generateKeys(options) {
-    return openpgp.generateKey(options).then(key => {
+    return openpgp.generateKey(options).then(function (key) {
         return {
             public_key: key.publicKeyArmored,
             private_key: key.privateKeyArmored,
