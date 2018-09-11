e0267e0abd888c0021cbdb2b1dc5deec419e5bf7
Zain Zafar <zain@aurorasolutions.io>
2018-09-12 01:04:01 +0500

    decryption with polyfills createAsyncFunction

--- src/assets/static/pgp-worker-encrypt.js
+++ src/assets/static/pgp-worker-encrypt.js
@@ -1,35 +1,103 @@
-'use strict'; // to make UMD bundles work
+// 'use strict'; // to make UMD bundles work
 
-self.window = {crypto: self.crypto}; // to make UMD bundles work
+self.window = { crypto: self.crypto || self.msCrypto }; // to make UMD bundles work
 
+importScripts('/polyfill.min.js');
+function createAsyncFunction(fn) {
+    return function () {
+        var gen = fn.apply(this, arguments);
+        return new Promise(function (resolve, reject) {
+        function step(key, arg) {
+            try {
+            var info = gen[key](arg);
+            var value = info.value;
+            } catch (error) {
+            reject(error);
+            return;
+            }
+            if (info.done) {
+            resolve(value);
+            } else {
+            return Promise.resolve(value).then(function (value) {
+                step("next", value);
+            }, function (err) {
+                step("throw", err);
+            });
+            }
+        }
+        return step("next");
+        });
+    };
+    }
 importScripts('/openpgp.min.js');
 var openpgp = window.openpgp;
-
-
-onmessage = function onmessage(event) {
-    if (event.data.encrypt) {
-        encryptContent(event.data.content, event.data.publicKeys).then(function (data) {
-            postMessage({ encryptedContent: data, encrypted: true, callerId: event.data.callerId });
-        });
-    } else if (event.data.encryptSecureMessageReply) {
-        encryptContent(event.data.content, event.data.publicKeys).then(function (data) {
-            postMessage({ encryptedContent: data, encryptSecureMessageReply: true });
-        });
-    }
-};
-
-function encryptContent (data, publicKeys) {
-    return new Promise(function(resolve, reject){
-        if (!data) {
-            resolve(null);
+function createAsyncFunction(fn) {
+    return function () {
+        var gen = fn.apply(this, arguments);
+        return new Promise(function (resolve, reject) {
+        function step(key, arg) {
+            try {
+            var info = gen[key](arg);
+            var value = info.value;
+            } catch (error) {
+            reject(error);
+            return;
+            }
+            if (info.done) {
+            resolve(value);
+            } else {
+            return Promise.resolve(value).then(function (value) {
+                step("next", value);
+            }, function (err) {
+                step("throw", err);
+            });
+            }
         }
-        const options = {
-            data: data,
-            publicKeys: publicKeys.map(function(item){ return openpgp.key.readArmored(item).keys[0];})
-        };
-        
-        openpgp.encrypt(options).then(function(ciphertext) {
-            resolve(ciphertext.data);
-        })
-    })
-}
+        return step("next");
+        });
+    };
+    }"use strict";
+
+    onmessage = function onmessage(event) {
+        if (event.data.encrypt) {
+            encryptContent(event.data.content, event.data.publicKeys).then(function (data) {
+                postMessage({ encryptedContent: data, encrypted: true, callerId: event.data.callerId });
+            });
+        } else if (event.data.encryptSecureMessageReply) {
+            encryptContent(event.data.content, event.data.publicKeys).then(function (data) {
+                postMessage({ encryptedContent: data, encryptSecureMessageReply: true });
+            });
+        }
+    };
+    
+    var encryptContent = createAsyncFunction( /*#__PURE__*/regeneratorRuntime.mark(function _callee(data, publicKeys) {
+        var options;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        if (data) {
+                            _context.next = 2;
+                            break;
+                        }
+    
+                        return _context.abrupt("return", null);
+    
+                    case 2:
+                        options = {
+                            data: data,
+                            publicKeys: publicKeys.map(function (item) {
+                                return openpgp.key.readArmored(item).keys[0];
+                            })
+                        };
+                        return _context.abrupt("return", openpgp.encrypt(options).then(function (ciphertext) {
+                            return ciphertext.data;
+                        }));
+    
+                    case 4:
+                    case "end":
+                        return _context.stop();
+                }
+            }
+        }, _callee, this);
+    }));
--- src/assets/static/pgp-worker.js
+++ src/assets/static/pgp-worker.js
@@ -1,79 +1,178 @@
-'use strict';
+"use strict"; // to make UMD bundles work
 
 self.window = { crypto: self.crypto || self.msCrypto }; // to make UMD bundles work
 
-importScripts('/openpgp.min.js');
+importScripts('/polyfill.min.js');
+importScripts('/openpgp.compat.js');
+
+function createAsyncFunction(fn) {
+    return function () {
+        var gen = fn.apply(this, arguments);
+        return new Promise(function (resolve, reject) {
+        function step(key, arg) {
+            try {
+            var info = gen[key](arg);
+            var value = info.value;
+            } catch (error) {
+            reject(error);
+            return;
+            }
+            if (info.done) {
+            resolve(value);
+            } else {
+            return Promise.resolve(value).then(function (value) {
+                step("next", value);
+            }, function (err) {
+                step("throw", err);
+            });
+            }
+        }
+        return step("next");
+        });
+    };
+}
+
 var openpgp = window.openpgp;
 
 var decryptedPrivKeyObj;
 var decryptedSecureMsgPrivKeyObj;
 
-
 onmessage = function onmessage(event) {
-    if (event.data.clear) {
-        decryptedPrivKeyObj = null;
-    }
-    else if (event.data.generateKeys) {
-        generateKeys(event.data.options).then(function (data) {
-            postMessage({generateKeys: true, keys: data, callerId: event.data.callerId, forEmail: !!event.data.forEmail});
-        })
-    }
-    else if (event.data.decryptSecureMessageKey) {
-	    decryptedSecureMsgPrivKeyObj = openpgp.key.readArmored(event.data.privKey).keys[0];
-	    decryptedSecureMsgPrivKeyObj.decrypt(event.data.password)
-		    .then(function (res) {
-			    postMessage({decryptSecureMessageKey: true, decryptedKey: decryptedSecureMsgPrivKeyObj});
-		    })
-		    .catch(function (error) {
-			    postMessage({ decryptSecureMessageKey: true, error: error.message});
-		    });
-    }
-    else if (event.data.decryptSecureMessageContent) {
-	    if (!event.data.content) {
-		    postMessage({decryptedContent: event.data.content, decryptSecureMessageContent: true});
-	    } else {
-		    decryptContent(event.data.content, decryptedSecureMsgPrivKeyObj).then(function (data) {
-			    postMessage({decryptedContent: data, decryptSecureMessageContent: true});
-		    })
-	    }
-    }
-    else if (!decryptedPrivKeyObj) {
-        decryptedPrivKeyObj = openpgp.key.readArmored(event.data.privkey).keys[0];
-        decryptedPrivKeyObj.decrypt(event.data.user_key);
-        postMessage({ key: decryptedPrivKeyObj});
-    }
-    else if (event.data.decrypt) {
-        if (!event.data.content) {
-            postMessage({decryptedContent: event.data.content, decrypted: true, callerId: event.data.callerId});
-        } else {
-            decryptContent(event.data.content, decryptedPrivKeyObj).then(function (data) {
-                postMessage({decryptedContent: data, decrypted: true, callerId: event.data.callerId});
-            })
+    initDecrypt(event);
+};
+
+var initDecrypt = createAsyncFunction( /*#__PURE__*/regeneratorRuntime.mark(function _callee(event) {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    if (!event.data.clear) {
+                        _context.next = 4;
+                        break;
+                    }
+
+                    decryptedPrivKeyObj = null;
+                    _context.next = 28;
+                    break;
+
+                case 4:
+                    if (!event.data.generateKeys) {
+                        _context.next = 8;
+                        break;
+                    }
+
+                    generateKeys(event.data.options).then(function (data) {
+                        postMessage({ generateKeys: true, keys: data, callerId: event.data.callerId, forEmail: !!event.data.forEmail });
+                    });
+                    _context.next = 28;
+                    break;
+
+                case 8:
+                    if (!event.data.decryptSecureMessageKey) {
+                        _context.next = 15;
+                        break;
+                    }
+
+                    _context.next = 11;
+                    return openpgp.key.readArmored(event.data.privKey);
+
+                case 11:
+                    decryptedSecureMsgPrivKeyObj = _context.sent.keys[0];
+
+                    decryptedSecureMsgPrivKeyObj.decrypt(event.data.password).then(function (res) {
+                        postMessage({ decryptSecureMessageKey: true, decryptedKey: decryptedSecureMsgPrivKeyObj });
+                    }).catch(function (error) {
+                        postMessage({ decryptSecureMessageKey: true, error: error.message });
+                    });
+                    _context.next = 28;
+                    break;
+
+                case 15:
+                    if (!event.data.decryptSecureMessageContent) {
+                        _context.next = 19;
+                        break;
+                    }
+
+                    if (!event.data.content) {
+                        postMessage({ decryptedContent: event.data.content, decryptSecureMessageContent: true });
+                    } else {
+                        decryptContent(event.data.content, decryptedSecureMsgPrivKeyObj).then(function (data) {
+                            postMessage({ decryptedContent: data, decryptSecureMessageContent: true });
+                        });
+                    }
+                    _context.next = 28;
+                    break;
+
+                case 19:
+                    if (decryptedPrivKeyObj) {
+                        _context.next = 27;
+                        break;
+                    }
+
+                    _context.next = 22;
+                    return openpgp.key.readArmored(event.data.privkey);
+
+                case 22:
+                    decryptedPrivKeyObj = _context.sent.keys[0];
+
+                    decryptedPrivKeyObj.decrypt(event.data.user_key);
+                    postMessage({ key: decryptedPrivKeyObj });
+                    _context.next = 28;
+                    break;
+
+                case 27:
+                    if (event.data.decrypt) {
+                        if (!event.data.content) {
+                            postMessage({ decryptedContent: event.data.content, decrypted: true, callerId: event.data.callerId });
+                        } else {
+                            decryptContent(event.data.content, decryptedPrivKeyObj).then(function (data) {
+                                postMessage({ decryptedContent: data, decrypted: true, callerId: event.data.callerId });
+                            });
+                        }
+                    }
+
+                case 28:
+                case "end":
+                    return _context.stop();
+            }
         }
-    }
-}
+    }, _callee, this);
+}));
 
-function decryptContent(data, privKeyObj) {
-    var options = {
-        message: openpgp.message.readArmored(data),
-        privateKeys: [privKeyObj]
-    };
+var decryptContent = createAsyncFunction( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(data, privKeyObj) {
+    var options;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+        while (1) {
+            switch (_context2.prev = _context2.next) {
+                case 0:
+                    _context2.next = 2;
+                    return openpgp.message.readArmored(data);
 
-    return openpgp.decrypt(options).then(function (plaintext) {
-        return plaintext.data;
-    }).catch(function (error) {
-        console.log(error);
-        return error;
-    })
-}
+                case 2:
+                    _context2.t0 = _context2.sent;
+                    _context2.t1 = [privKeyObj];
+                    options = {
+                        message: _context2.t0,
+                        privateKeys: _context2.t1
+                    };
+                    return _context2.abrupt("return", openpgp.decrypt(options).then(function (plaintext) {
+                        return plaintext.data;
+                    }));
+
+                case 6:
+                case "end":
+                    return _context2.stop();
+            }
+        }
+    }, _callee2, this);
+}));
 
 function generateKeys(options) {
-    return openpgp.generateKey(options).then(function (key) {
-        return {
-            public_key: key.publicKeyArmored,
-            private_key: key.privateKeyArmored,
-            fingerprint: openpgp.key.readArmored(key.publicKeyArmored).keys[0].primaryKey.getFingerprint()
-        };
-    });
+    // return openpgp.generateKey(options).then(key => {
+    //     return {
+    //         public_key: key.publicKeyArmored,
+    //         private_key: key.privateKeyArmored,
+    //         fingerprint: openpgp.key.readArmored(key.publicKeyArmored).keys[0].primaryKey.getFingerprint()
+    //     };
+    // });
 }
-
